fdecea487debf6d50807c4e5370be4e8f8ce3cb0
Mieartt <ard.iheb@gmail.com>
2025-02-02 16:54:42 +0100

    jhgjhgkhkj

--- AISSA.vhd
+++ AISSA.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity aissa is
  port (
-        fom    : in std_logic;
-        khcham : out std_logic
+        fom    :      in std_logic;
+        khcham :      out std_logic
  );
  end entity;
  architecture behavior of aissa is 
